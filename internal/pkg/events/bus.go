bb7fd64714aef5eb5a8c2bad975caba8063661b7
Ryan Baker <rbaker@xtier.com>
2026-01-19 12:59:13 -0500

    Verify events package

--- internal/pkg/events/bus.go
+++ internal/pkg/events/bus.go
@@ -7,27 +7,31 @@ import (
 	"sync"
 )
 
-type Handler func(ctx context.Context, event domain.DomainEvent) error
+type EventHandler func(ctx context.Context, event domain.Event) error
 
 type EventBus interface {
-	Subscribe(eventName string, handler Handler)
-	Publish(ctx context.Context, event domain.DomainEvent) error
+	Subscribe(eventName string, handler EventHandler)
+	Publish(ctx context.Context, event domain.Event) error
 }
+
+var (
+    _ EventBus = (*InMemoryEventBus)(nil)
+)
 
 type InMemoryEventBus struct {
 	mu       sync.RWMutex
-	handlers map[string][]Handler
+	handlers map[string][]EventHandler
 	logger   *slog.Logger
 }
 
 func NewInMemoryEventBus(logger *slog.Logger) *InMemoryEventBus {
 	return &InMemoryEventBus{
-		handlers: make(map[string][]Handler),
+		handlers: make(map[string][]EventHandler),
 		logger:   logger,
 	}
 }
 
-func (b *InMemoryEventBus) Subscribe(eventName string, handler Handler) {
+func (b *InMemoryEventBus) Subscribe(eventName string, handler EventHandler) {
 	b.mu.Lock()
 	defer b.mu.Unlock()
 
@@ -38,7 +42,7 @@ func (b *InMemoryEventBus) Subscribe(eventName string, handler Handler) {
 	)
 }
 
-func (b *InMemoryEventBus) Publish(ctx context.Context, event domain.DomainEvent) error {
+func (b *InMemoryEventBus) Publish(ctx context.Context, event domain.Event) error {
 	b.mu.RLock()
 	handlers := b.handlers[event.EventName()]
 	b.mu.RUnlock()
@@ -67,88 +71,4 @@ func (b *InMemoryEventBus) Publish(ctx context.Context, event domain.DomainEvent
 	}
 
 	return nil
-}
-
-type eventJob struct {
-	ctx   context.Context
-	event domain.DomainEvent
-}
-
-type AsyncEventBus struct {
-	*InMemoryEventBus
-	nWorkers int
-	queue    chan eventJob
-	wg       sync.WaitGroup
-	ctx      context.Context
-	cancel   context.CancelFunc
-}
-
-func NewAsyncEventBus(logger *slog.Logger, nWorkers int) *AsyncEventBus {
-	ctx, cancel := context.WithCancel(context.Background())
-
-	bus := &AsyncEventBus{
-		InMemoryEventBus: NewInMemoryEventBus(logger),
-		nWorkers:         nWorkers,
-		queue:            make(chan eventJob, 100),
-		ctx:              ctx,
-		cancel:           cancel,
-	}
-
-	for i := 0; i < nWorkers; i++ {
-		bus.wg.Add(1)
-		go bus.worker(i)
-	}
-
-	return bus
-}
-
-func (b *AsyncEventBus) worker(id int) {
-	defer b.wg.Done()
-	b.logger.Debug("event worker started", "worker_id", id)
-
-	for {
-		select {
-		case <-b.ctx.Done():
-			b.logger.Debug("event worker stopped", "worker_id", id)
-			return
-
-		case job := <-b.queue:
-			b.logger.Debug("worker processing event",
-				"worker_id", id,
-				"event", job.event.EventName(),
-			)
-
-			if err := b.InMemoryEventBus.Publish(job.ctx, job.event); err != nil {
-				b.logger.Error("worker failed to process event",
-					"worker_id", id,
-					"event", job.event.EventName(),
-					"error", err,
-				)
-			}
-		}
-	}
-}
-
-func (b *AsyncEventBus) Publish(ctx context.Context, event domain.DomainEvent) error {
-	select {
-	case b.queue <- eventJob{
-		ctx:   ctx,
-		event: event,
-	}:
-		b.logger.Debug("event enqueued",
-			"event", event.EventName(),
-			"queue_length", len(b.queue),
-		)
-		return nil
-
-	case <-b.ctx.Done():
-		return b.ctx.Err()
-	}
-}
-
-func (b *AsyncEventBus) Shutdown() {
-	b.cancel()
-	close(b.queue)
-	b.wg.Wait()
-	b.logger.Info("async event bus shutdown complete")
 }

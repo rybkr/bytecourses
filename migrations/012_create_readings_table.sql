c87fa75f5a87ce5afacaf36126decc42bc3776f9
Ryan Baker <rbaker@xtier.com>
2026-01-23 11:40:36 -0500

    Add migrations for modules and readings

---
+++ migrations/012_create_readings_table.sql
@@ -0,0 +1,21 @@
+-- +goose Up
+-- +goose StatementBegin
+DO $$
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'reading_format') THEN
+        CREATE TYPE reading_format AS ENUM (
+            'markdown'
+        );
+    END IF;
+END $$;
+-- +goose StatementEnd
+
+CREATE TABLE IF NOT EXISTS readings (
+    content_item_id BIGINT PRIMARY KEY REFERENCES content_items(id) ON DELETE CASCADE,
+    format          reading_format NOT NULL,
+    content         TEXT
+);
+
+-- +goose Down
+DROP TABLE IF EXISTS readings;
+DROP TYPE IF EXISTS reading_format;
